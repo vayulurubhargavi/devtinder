0f40d754be87325c2af9fed139c01320c3b7500e
vayulurubhargavi <vayuluru16@gmail.com>
2025-05-07 22:41:06 +0530

    expolring routing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,38 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
   res.send("namaste World!");
 });
 
-app.get("/dashboard", (req, res) => {
-  res.send("this is dashboard!.....");
+// app.use("/hello/xyz", (req, res) => {
+//   res.send("hello hello xyz!.....");
+// });
+
+app.get("/user/:userid/:name/:password", (req, res) => {
+  console.log(req.params,'===> req.params');
+  res.send({ firstname: "Akshay", lastname: "kumar", age: "35" });
 });
+
+app.post("/user", (req, res) => {
+  //saving data to db
+  res.send("Data saved to DB successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted from DB successfully!");
+});
+// app.use("/hello", (req, res) => {
+//   res.send("hello hello heelloo!.....");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("teszt server !.....");
+// });
+
+// app.use("/dashboard", (req, res) => {
+//   res.send("this is dashboard!.....");
+// });
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
